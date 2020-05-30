8db83b91a3cb932872be39f7280c135ff9f3c5c9
Maliex <malikopeyemi5@gmail.com>
2020-05-31 00:45:42 +0100

    '2'

--- app/app.py
+++ app/app.py
@@ -1,17 +1,9 @@
 from flask import Flask, render_template, request
-from flask_mail import Mail, Message
+
 
 app = Flask(__name__)
 
-app.config.update(dict(
-	DEBUG =True,
-	MAIL_SERVER = 'smtp.gmail.com',
-	MAIL_PORT = 587,
-	MAIL_USE_TLS = True,
-	MAIL_USE_SSL =False,
-	MAIL_USERNAME = '',
-	MAIL_PASSWORD = '',
-))
+
 
 mail  = Mail(app)
 
@@ -21,19 +13,8 @@ mail  = Mail(app)
 def index():
 	return render_template('index.html')
 
-@app.route('/contact', methods=['POST'])
-def contact():
-	email = request.form['email']
-	name = request.form['name']
-	subject = request.form['subject']
-	body = request.form['message']
+#@app.route('/contact', methods=['POST'])
 
-	msg = Message(subject, sender=email, recipients=['malikopeyemi5@gmail.com'])
-	msg.body = body
-	if (mail.send(msg)):
-		return 'OK'
-	else:
-		return 'Mail not sent. Try again'
 
 if __name__ == '__main__':
 	app.run(debug=True)
